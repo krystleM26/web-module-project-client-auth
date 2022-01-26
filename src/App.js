7307572a5f400d78b79eab2d38a788654f1e2603
Krystle Mitchell <mitchell.krystle2@gmail.com>
2022-01-25 20:41:42 -0800

    Login renders and routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import Login from './components/Login'
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <h2>Client Auth Project</h2>
+      <h2>Friends Database</h2>
+      <nav>
+
+      <Link to="/login">Login</Link>
+      </nav>
+     
+
     </div>
+      </Router>
   );
 }
 
---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      <form>
+        <input type="text" name="name" />
+      </form>
+    </div>
+  )
+}
+
+export default Login
